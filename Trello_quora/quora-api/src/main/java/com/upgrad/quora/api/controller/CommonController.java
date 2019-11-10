284afc29b4bf0c89f2e4c198c768fef7517c4da1
praveen <pk.251094@gmail.com>
2019-11-10 11:08:48 +0530

    common controller implemented with changes in userservice and userdao

---
+++ Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
@@ -0,0 +1,37 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.UserDetailsResponse;
+import com.upgrad.quora.service.business.UserAdminBusinessService;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/userprofile")
+public class CommonController {
+
+    @Autowired
+    private UserAdminBusinessService userAdminBusinessService;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{userId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<UserDetailsResponse> userDetails(@PathVariable("userId") String uuid, @RequestHeader("authorization") final String authorization) throws UserNotFoundException, AuthorizationFailedException {
+
+        final UserDetailsResponse userDetailsResponse=new UserDetailsResponse();
+
+        String accessToken = authorization.startsWith("Bearer ")?authorization.split("Bearer ")[1]:authorization;
+        UserEntity userEntity = userAdminBusinessService.getUser(uuid,accessToken);
+
+        userDetailsResponse.firstName(userEntity.getFirstName()).lastName(userEntity.getLastName()).userName(userEntity.getUserName())
+                            .emailAddress(userEntity.getEmail()).country(userEntity.getCountry()).aboutMe(userEntity.getAboutMe())
+                            .dob(userEntity.getDob()).contactNumber(userEntity.getContactNumber());
+
+        return new ResponseEntity<UserDetailsResponse>(userDetailsResponse, HttpStatus.OK);
+
+    }
+
+}
